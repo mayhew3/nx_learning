103c91ddb5f369c95e8322add9f69f047b603700
Mayhew Seavey <commander.boffo@gmail.com>
2022-11-03 17:05:40 -0700

    Step 3b: All tests pass

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -11,4 +11,10 @@ interface Todo {
 })
 export class AppComponent {
   todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
 }
