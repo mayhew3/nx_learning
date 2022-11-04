6ee00806cc791a86c47195b451dcbad6e6912fe8
Mayhew Seavey <commander.boffo@gmail.com>
2022-11-03 17:02:03 -0700

    Step 3a: One test passes

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+}
+
 @Component({
   selector: 'myorg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
 }
